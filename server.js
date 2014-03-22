0344da122dd432bbd6c95087b80f72b68866d17c
Jordan Burke <me@jordankburke.com>
2014-03-21 21:07:40 -0400

    Created script.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-var jade = require('jade');
-var express = require('express'), app = express.createServer();
+var express = require('express')
+  , http = require('http');
+
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -12,3 +16,17 @@ app.get('/', function(req, res){
   res.render('index.jade');
 });
 app.listen(3000);
+
+io.sockets.on('connection', function (socket) {
+  socket.on('setPseudo', function (data) {
+    socket.set('pseudo', data);
+  });
+
+  socket.on('message', function (message) {
+    socket.get('pseudo', function (error, name) {
+        var data = { 'message' : message, pseudo : name };
+        socket.broadcast.emit('message', data);
+        console.log("user " + name + " send this : " + message);
+    })
+  });
+});
