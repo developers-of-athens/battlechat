6bf55b1bc64033a3efcd877350ee341ae103c073
Jordan Burke <me@jordankburke.com>
2014-03-21 22:57:13 -0400

    Removed logfmt dependency

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ var appPort = 16558;
 // Librairies
 
 var express = require('express'), app = express();
-
-var logfmt = require("logfmt");
 var http = require('http')
   , server = http.createServer(app)
   , io = require('socket.io').listen(server);
